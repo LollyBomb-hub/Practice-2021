b1c3b668f778858e961ab645889058a6865c9c73
LollyBomb-hub <34553831+LollyBomb-hub@users.noreply.github.com>
2021-07-10 23:42:03 +0300

    LibGUI commit

---
+++ libGUI/libGUI.cpp
@@ -0,0 +1,2 @@
+#include "libGUI.h"
+
---
+++ libGUI/libGUI.h
@@ -0,0 +1,12 @@
+#ifndef _GUI_DLL_H_
+#define _GUI_DLL_H_
+
+#if BUILDING_DLL
+#define DLLIMPORT __declspec(dllexport)
+#else
+#define DLLIMPORT __declspec(dllimport)
+#endif
+
+
+
+#endif
